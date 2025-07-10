0c4903dcce60717ad4d53de57c975c6d3c0a9312
aamirhasan1415 <aamirhasan2929@gmail.com>
2025-07-10 09:26:13 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,18 +1,35 @@
-export default function Register () {
-       return (
-        <>
-        <form action=""></form>
+export default function Register() {
+  return (
+    <>
+      <form action="#">
         <fieldset>
-            <legend>Register Form</legend>
-        <p><label htmlFor="FName">FName:</label>
-        <input type="text" /></p>
-        <p><label htmlFor="FName">FName:</label>
-        <input type="text" /></p>
-        <p><label htmlFor="email">Email:</label>
-        <input type="email" /></p>
-        <p><label htmlFor="password">password</label>
-        <input type="password" /></p>
+          <legend>Register Form</legend>
+
+          <p>
+            <label htmlFor="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" required />
+          </p>
+
+          <p>
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" required />
+          </p>
+
+          <p>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </p>
+
+          <p>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </p>
+
+          <p>
+            <button type="submit">Register</button>
+          </p>
         </fieldset>
-        </>
-       )
+      </form>
+    </>
+  );
 }
