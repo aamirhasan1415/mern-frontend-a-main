fbd4086d91a7f2a5d1b4edf08c7faa56deb011e5
aamirhasan1415 <aamirhasan2929@gmail.com>
2025-07-17 09:38:15 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "./Header.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import App, { AppContext } from "../App";
 export default function Header() {
   const { user } = useContext(AppContext);
   return (
-    <div>
-      <h1>MERN Frontend</h1>
+    <div className="navbar">
+      <h1 className="navbar-title">MERN Frontend</h1>
+      <div className="navbar-links">
       <Link to="/">Home</Link>-<Link to="/cart">MyCart</Link>-
       <Link to="/order">MyOrder</Link>
       {/* <Link to="/admin">Admin</Link> */}
@@ -15,7 +17,7 @@ export default function Header() {
       
       {user?.token ? <Link to="/profile">Profile</Link> : <Link to="/login">Login</Link> }
 
-
+      </div>
     </div>
   );
 }
