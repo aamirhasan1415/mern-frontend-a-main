8a4e123f833bdca5dbec69ad5126ff6018114f98
aamirhasan1415 <aamirhasan2929@gmail.com>
2025-07-21 09:28:16 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
+import { toast } from "react-toastify";
+
 import axios from "axios";
 import { AppContext } from "../App";
+import "./Product.css";
 export default function Product() {
   const API_URL = import.meta.env.VITE_API_URL;
   const [products, setProducts] = useState([]);
@@ -25,18 +28,22 @@ export default function Product() {
     if (!found) {
       product.qty = 1;
       setCart([...cart, product]);
+      toast.success("Added to cart!");
+    }
+    else{
+      toast.info("Already in cart");
     }
   };
   return (
-    <div>
+    <div className="product-list-container">
       {products &&
         products.map((product) => (
-          <div key={product._id}>
-            <img src={product.imgUrl} width={100}/>
-            <h3>{product.productName}</h3>
-            <p>{product.description}</p>
-            <h4>{product.price}</h4>
-            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          <div key={product._id} className="product-card">
+            <img src={product.imgUrl} width={100} className="product-img"/>
+            <h3 className="product-title">{product.productName}</h3>
+            <p className="product-desc">{product.description}</p>
+            <h4 className="product-price">{product.price}</h4>
+            <button className="product-add-btn" onClick={() => addToCart(product)}>Add to Cart</button>
           </div>
         ))}
     </div>
